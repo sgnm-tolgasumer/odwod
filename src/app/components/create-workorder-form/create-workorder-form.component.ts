b6568911f575212cfce547c5583ccf6fce4faf82
Berk Önder <berk.onder@ug.bilkent.edu.tr>
2021-02-28 00:50:12 +0300

    backend for create work order and signup customer forms

--- src/app/components/create-workorder-form/create-workorder-form.component.ts
+++ src/app/components/create-workorder-form/create-workorder-form.component.ts
@@ -1,4 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { NgForm } from '@angular/forms';
+
+interface City {
+  value: string;
+  viewValue: string;
+}
+
+interface Region {
+  valueregion: string;
+  viewValueregion: string;
+}
+
+interface Type {
+  valuetype: string;
+  viewValuetype: string;
+}
 
 @Component({
   selector: 'app-create-workorder-form',
@@ -6,10 +23,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-workorder-form.component.css']
 })
 export class CreateWorkorderFormComponent implements OnInit {
+  
+  selectedValue: string;
+  selectedValueregion: string;
+  selectedValuetype: string;
 
-  constructor() { }
+  cities: City[] = [
+    {value: 'Ankara', viewValue: 'Ankara'},
+  ];
+
+  regions: Region[] = [
+    {valueregion: 'Cankaya', viewValueregion: 'Cankaya'},
+    {valueregion: 'Yenimahalle', viewValueregion: 'Yenimahalle'},
+    {valueregion: 'Kazan', viewValueregion: 'Kazan'}
+  ];
+
+  types: Type[] = [
+    {valuetype: 'Temizlik', viewValuetype: 'Temizlik'},
+    {valuetype: 'Güvenlik', viewValuetype: 'Güvenlik'}
+  ];
+
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm){
+
+    const url='http://localhost:8080/';
+    this.http.post(url,f.value)
+    .subscribe(
+      (result)=>{
+        this.ngOnInit();
+      }
+    );
+    console.log(f.value);
   }
 
 }
