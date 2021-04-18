7341b35a4011e2815871a9542a643e7d51143ab1
Seyit Bahadir Cetin <34353055+bahadircetin@users.noreply.github.com>
2021-04-19 01:39:09 +0300

    -Close Work Order button is active now. -It will send a request to server via REST API to transfer work order which is in_progress to done.

--- src/app/components/worker-current-job/worker-current-job.component.ts
+++ src/app/components/worker-current-job/worker-current-job.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkorderTransferService } from './../../shared/services/workorder-transfer.service';
 import { AuthService } from "../../shared/services/auth.service";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Component({
   selector: 'app-worker-current-job',
@@ -9,10 +16,19 @@ import { AuthService } from "../../shared/services/auth.service";
 })
 export class WorkerCurrentJobComponent implements OnInit {
   workOrder: any;
-  constructor(private transfered: WorkorderTransferService, public authService: AuthService) { }
+  constructor(private transfered: WorkorderTransferService, public authService: AuthService, private http: HttpClient ) { }
 
   ngOnInit(): void {
     this.workOrder = this.transfered.workOrderToTransfer.subscribe(message => this.workOrder = message);
 
+  }
+  /**
+   * It will transfer the workorder from Kafka's in_progress topic to done topic via REST API.
+   */
+  public closeJobButtonClick(workOrder: any){
+    this.http.post("http://localhost:8080/workorder/transfer/" + workOrder["workOrderId"] + "/" + "in_progress"+"/done/" + workOrder["workerId"], httpOptions).subscribe(data =>
+    {
+
+    });
   }
 }
