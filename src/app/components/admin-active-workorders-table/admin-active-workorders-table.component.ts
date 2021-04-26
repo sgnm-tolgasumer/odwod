481c2b9b5e79f133888fb25806daf7bc5b61dc69
Seyit Bahadir Cetin <34353055+bahadircetin@users.noreply.github.com>
2021-04-26 23:01:09 +0300

    Active workorders table component created.

---
+++ src/app/components/admin-active-workorders-table/admin-active-workorders-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-active-workorders-table',
+  templateUrl: './admin-active-workorders-table.component.html',
+  styleUrls: ['./admin-active-workorders-table.component.css']
+})
+export class AdminActiveWorkordersTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
